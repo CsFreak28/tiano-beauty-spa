24e4ca2ee27089565ffc3e2c04b12c30072e4bb5
Gideon uwa <uwagideon092@gmail.com>
2022-09-06 23:44:08 -0700

    made_discount_date_dynamic

--- src/components/bookComp/bookComp.tsx
+++ src/components/bookComp/bookComp.tsx
@@ -2,15 +2,37 @@ import Styles from "./bookComp.module.scss";
 import BookButton from "./bookButton";
 import { ReactComponent as BookArrow } from "../../assets/svgs/bookArrow.svg";
 import { DatePick } from "./datePick";
+import { useState } from "react";
 const BookComp = () => {
-  
+  const [showDateInput, setShowDateInput] = useState<boolean>(false);
+  function getDiscountDate(): string {
+    let currentDate = new Date();
+    let todaysDate = currentDate.getDate() + 1;
+    // console.log(currentDate.setDate(10));
+    let currentDateToNumber = new Date(currentDate.setDate(10)).getDate();
+    let discountDate = new Date(
+      currentDate.setDate(currentDateToNumber + todaysDate)
+    );
+    let discountDateLocaleString = discountDate.toLocaleDateString();
+    return discountDateLocaleString;
+  }
+  let discountDate = getDiscountDate();
+  function toggleShowDateInput() {
+    setShowDateInput((prev) => !prev);
+  }
   return (
     <div className={Styles.bookContainer}>
       <div className={Styles.bookElement}>
         <div>
           <h6>Arrival date</h6>
-          <p>
-            pick now <BookArrow />
+          <p onClick={toggleShowDateInput}>
+            {showDateInput ? (
+              <DatePick />
+            ) : (
+              <>
+                pick now <BookArrow />
+              </>
+            )}
           </p>
         </div>
         <div className={Styles.line}></div>
@@ -18,7 +40,7 @@ const BookComp = () => {
       <div className={Styles.bookElement}>
         <div>
           <h6>Next discount</h6>
-          <p>28/10/2022</p>
+          <p>{discountDate}</p>
         </div>
         <div className={Styles.line}></div>
       </div>
@@ -29,8 +51,17 @@ const BookComp = () => {
             pick now <BookArrow />
           </p>
         </div>
+        <div className={Styles.line}></div>
       </div>
-      <BookButton text="BOOK NOW"/>
+      <div className={Styles.bookElement}>
+        <div>
+          <h6>Your email</h6>
+          <p>
+            pick now <BookArrow />
+          </p>
+        </div>
+      </div>
+      <BookButton text="BOOK NOW" />
     </div>
   );
 };
