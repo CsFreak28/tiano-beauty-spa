c56420a484694e462ef9c78c438752c3de470a2e
Gideon uwa <uwagideon092@gmail.com>
2022-09-12 05:50:14 -0700

    added_booking-anonymously-to-appointment-popup

--- src/components/bookComp/bookButton.tsx
+++ src/components/bookComp/bookButton.tsx
@@ -21,10 +21,10 @@ const BookButton = (props: ButtonProps) => {
         props.Styles ? props.Styles.bookButton : Styles.bookButton
       }`}
       onClick={() => {
-        if (props.bookStraight) {
+        // if (props.bookStraight) {
           props.appointmentDetails &&
             bookAppointmentAnonymously(props.appointmentDetails);
-        }
+        // }
       }}
     >
       {props.text}
--- src/components/popupBook/popupBook.tsx
+++ src/components/popupBook/popupBook.tsx
@@ -71,65 +71,78 @@ export const AppointmentPopup = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <div className={Styles.container}>
-      <div className={Styles.innerContainer}>
-        <p className={Styles.heading}>book an appointment</p>
-        <div className={Styles.questionsContainer}>
-          <div className={Styles.question}>
-            <p>1.</p>
-            <p>
-              which day would you <br /> like to come for the <br />{" "}
-              appointment?
-            </p>
-            <DatePick setDate={setDate} />
-          </div>
-          <div className={Styles.question}>
-            <p>2.</p>
-            <p>
-              which service would <br /> you like to book an <br /> appointment
-              for?
-            </p>
-            <ServiceDropDown
-              DropDownDetails={DropDownDetails}
-              setDropDownDetails={setDropDownDetails}
-              DropDownStyles={Styles}
-            />
-          </div>
-          <div className={Styles.question}>
-            <p>3.</p>
-            <p>
-              how many people <br /> would you like to book <br /> an{" "}
-              appointment with?
-            </p>
-            <Counter
-              CounterStyles={Styles}
-              numberOfPeople={state.numberOfPeople}
-              updateNumberOfPeople={dispatch}
-            />
-          </div>
-          <div className={Styles.question}>
-            <p>3.</p>
-            <p>
-              please provide us
-              <br /> with an email
-            </p>
-            <div>
-              <input
-                type="text"
-                placeholder="uwagideon092@gm..."
-                className={Styles.emailInput}
-                value={state.email}
-                onChange={(e) => {
-                  dispatch({ type: "updateEmail", payload: e.target.value });
-                }}
+    <>
+      <div className={Styles.layerBlur}></div>
+      <div className={Styles.container}>
+        <div className={Styles.innerContainer}>
+          <p className={Styles.heading}>book an appointment</p>
+          <div className={Styles.questionsContainer}>
+            <div className={Styles.question}>
+              <p>1.</p>
+              <p>
+                which day would you <br /> like to come for the <br />{" "}
+                appointment?
+              </p>
+              <DatePick setDate={setDate} />
+            </div>
+            <div className={Styles.question}>
+              <p>2.</p>
+              <p>
+                which service would <br /> you like to book an <br />{" "}
+                appointment for?
+              </p>
+              <ServiceDropDown
+                DropDownDetails={DropDownDetails}
+                setDropDownDetails={setDropDownDetails}
+                DropDownStyles={Styles}
               />
             </div>
+            <div className={Styles.question}>
+              <p>3.</p>
+              <p>
+                how many people <br /> would you like to book <br /> an{" "}
+                appointment with?
+              </p>
+              <Counter
+                CounterStyles={Styles}
+                numberOfPeople={state.numberOfPeople}
+                updateNumberOfPeople={dispatch}
+              />
+            </div>
+            <div className={Styles.question}>
+              <p>4.</p>
+              <p>
+                please provide us
+                <br /> with an email
+              </p>
+              <div>
+                <input
+                  type="text"
+                  placeholder="uwagideon092@gm..."
+                  className={Styles.emailInput}
+                  value={state.email}
+                  onChange={(e) => {
+                    dispatch({ type: "updateEmail", payload: e.target.value });
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={Styles.bookButtonContainer}>
+            <BookButton
+              text="Book an appointment now"
+              bookStraight = {false}
+              appointmentDetails={{
+                bookedOn: new Date().toDateString(),
+                service: DropDownDetails.currentChosenService,
+                email: state.email,
+                numberOfPeople: state.numberOfPeople,
+                appointmentDate: state.appointmentDate,
+              }}
+            />
           </div>
         </div>
-        <div className={Styles.bookButtonContainer}>
-          <BookButton text="Book an appointment now" bookStraight />
-        </div>
       </div>
-    </div>
+    </>
   );
 };
