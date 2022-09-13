74eda421ed2b99c80c0673a44abb7ee19c6d01f9
Gideon uwa <uwagideon092@gmail.com>
2022-09-13 05:41:11 -0700

    optimized_FCP-by-removing-second-image

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -11,77 +11,76 @@ const HeroSection = () => {
       <div className={Styles.split}>
         <div className={Styles.copyText}>
           <p>
-
-          <span>r</span>
-          <span>e</span>
-          <span>l</span>
-          <span>a</span>
-          <span>x</span>
-          <span>,</span>
-          <span className={Styles.whitespace}></span>
-          <span>r</span>
-          <span>e</span>
-          <span>j</span>
-          <span>u</span>
-          <span>v</span>
-          <span>e</span>
-          <span>n</span>
-          <span>a</span>
-          <span>t</span>
-          <span>e</span>
-          <span>,</span>
-          <br />
-          <span>r</span>
-          <span>e</span>
-          <span>f</span>
-          <span>r</span>
-          <span>e</span>
-          <span>s</span>
-          <span>h</span>
-          <span className={Styles.whitespace}></span>
-          <span className={Styles.whitespace}></span>
-          <span className={Styles.whitespace}></span>
-          <span>a</span>
-          <span>n</span>
-          <span>d</span>
-          <br />
-          <span>m</span>
-          <span>o</span>
-          <span>s</span>
-          <span>t</span>
-          <span className={Styles.whitespace}></span>
-          <span>e</span>
-          <span>s</span>
-          <span>p</span>
-          <span>e</span>
-          <span>c</span>
-          <span>i</span>
-          <span>a</span>
-          <span>l</span>
-          <span>ly</span>
-          <br />
-          <span>enj</span>
-          <span>o</span>
-          <span>y</span>
-          <span className={Styles.whitespace}></span>
-          <span>you</span>
-          <span>r</span>
-          <span>s</span>
-          <span>e</span>
-          <span>l</span>
-          <span>f</span>
-          <span>...</span>
+            <span>r</span>
+            <span>e</span>
+            <span>l</span>
+            <span>a</span>
+            <span>x</span>
+            <span>,</span>
+            <span className={Styles.whitespace}></span>
+            <span>r</span>
+            <span>e</span>
+            <span>j</span>
+            <span>u</span>
+            <span>v</span>
+            <span>e</span>
+            <span>n</span>
+            <span>a</span>
+            <span>t</span>
+            <span>e</span>
+            <span>,</span>
+            <br />
+            <span>r</span>
+            <span>e</span>
+            <span>f</span>
+            <span>r</span>
+            <span>e</span>
+            <span>s</span>
+            <span>h</span>
+            <span className={Styles.whitespace}></span>
+            <span className={Styles.whitespace}></span>
+            <span className={Styles.whitespace}></span>
+            <span>a</span>
+            <span>n</span>
+            <span>d</span>
+            <br />
+            <span>m</span>
+            <span>o</span>
+            <span>s</span>
+            <span>t</span>
+            <span className={Styles.whitespace}></span>
+            <span>e</span>
+            <span>s</span>
+            <span>p</span>
+            <span>e</span>
+            <span>c</span>
+            <span>i</span>
+            <span>a</span>
+            <span>l</span>
+            <span>ly</span>
+            <br />
+            <span>enj</span>
+            <span>o</span>
+            <span>y</span>
+            <span className={Styles.whitespace}></span>
+            <span>you</span>
+            <span>r</span>
+            <span>s</span>
+            <span>e</span>
+            <span>l</span>
+            <span>f</span>
+            <span>...</span>
           </p>
           <div className={Styles.bookButtonContainer}>
-            <NotNowButton Styles={Styles}/>
-            <BookButton Styles={Styles} text='BOOK NOW'/>
+            <NotNowButton Styles={Styles} />
+            <BookButton Styles={Styles} text="BOOK NOW" bookStraight={false} />
           </div>
         </div>
         <div className={Styles.imgHolder}>
           <img src={HeroImg} alt="" className={Styles.heroImg} />
-          <img src={SecondHeroImg} alt="" className={Styles.otherHeroImg} />
+          <img src={HeroImg} alt="" className={Styles.secondHeroImg} />
         </div>
-        <BookComp />  
+        <BookComp />
       </div>
     </div>
   );
