0871a474632d8ed80bc8ca1907ae2e9537a30f04
Gideon uwa <uwagideon092@gmail.com>
2022-09-14 21:44:28 -0700

    removed_themecontext-from-appContext

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,18 @@ import DashBoard from "./pages/dashboard/dashboard";
 import ProtectedRoutes from "./pages/auth/protectedRoutes";
 import { SettingsPage } from "./pages/dashboard/dashboardComponents2";
 import { FourOFourPage } from "./components/404Page";
+import { AppointmentPopup } from "./components/popupBook/popupBook";
 import {
   ProfileOverview,
   NotficationPage,
 } from "./pages/dashboard/dashboardComponents";
+export const ThemeContext = React.createContext(false);
 function App() {
   let mobileNavRef = useRef<any>(null);
   let hamburgerMenu = useRef<any>(null);
-  // hamburgerMenu.current.style.backgroundColor = "blue";
   const [menuState, setMenuState] = useState<boolean>(false);
+  const [showAppointmentPopup, setShowAppointmentPopup] =
+    useState<boolean>(false);
   function removeMobileNavBar() {
     if (hamburgerMenu.current !== null) {
       let arrayOfChildren: Array<HTMLElement> = [];
@@ -37,7 +40,7 @@ function App() {
       setMenuState((oldState) => !oldState);
     }
   }
-
+  const [closeElements, setCloseElements] = useState<boolean>(false);
   useEffect(() => {
     if (visualViewport !== null) {
       visualViewport.addEventListener("resize", (e) => {
@@ -50,53 +53,68 @@ function App() {
   return (
     <div className="App">
       {/* <div className="background"></div> */}
-      <div ref={mobileNavRef} className="mobileNav">
-        <div className="innerContainer">
-          <Link to="/auth/signUp">
-            <div className="navElement" onClick={removeMobileNavBar}>
-              sign up
-            </div>
-          </Link>{" "}
-          <Link to="/auth/signIn">
-            <div className="navElement" onClick={removeMobileNavBar}>
-              sign in
-            </div>
-          </Link>
-          <Link to="/">
-            {" "}
-            <div className="navElement" onClick={removeMobileNavBar}>
-              home
-            </div>
-          </Link>
-          <Link to="/aboutus" onClick={removeMobileNavBar}>
-            <div className="navElement">about us</div>
-          </Link>
-        </div>
+      <div>
+        <button
+          onClick={() => {
+            setShowAppointmentPopup((prev) => !prev);
+          }}
+        >
+          open appointment popup
+        </button>
+        {showAppointmentPopup && <AppointmentPopup />}
       </div>
-      <Navbar
-        mobileNav={mobileNavRef}
-        random={hamburgerMenu}
-        menuState={menuState}
-      />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/aboutus" element={<AboutPage />} />
-        <Route path="/auth/:typeOfAuth" element={<Authenticate />} />
-        <Route element={<ProtectedRoutes />}>
-          <Route element={<DashBoard />}>
-            <Route
-              path="/auth/dashboard/profile"
-              element={<ProfileOverview />}
-            />
-            <Route
-              path="/auth/dashboard/notifications"
-              element={<NotficationPage />}
-            />
-            <Route path="/auth/dashboard/settings" element={<SettingsPage />} />
+      <ThemeContext.Provider value={closeElements}>
+        <div ref={mobileNavRef} className="mobileNav">
+          <div className="innerContainer">
+            <Link to="/auth/signUp">
+              <div className="navElement" onClick={removeMobileNavBar}>
+                sign up
+              </div>
+            </Link>{" "}
+            <Link to="/auth/signIn">
+              <div className="navElement" onClick={removeMobileNavBar}>
+                sign in
+              </div>
+            </Link>
+            <Link to="/">
+              {" "}
+              <div className="navElement" onClick={removeMobileNavBar}>
+                home
+              </div>
+            </Link>
+            <Link to="/aboutus" onClick={removeMobileNavBar}>
+              <div className="navElement">about us</div>
+            </Link>
+          </div>
+        </div>
+        <Navbar
+          mobileNav={mobileNavRef}
+          random={hamburgerMenu}
+          menuState={menuState}
+        />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/aboutus" element={<AboutPage />} />
+          <Route path="/auth/:typeOfAuth" element={<Authenticate />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route element={<DashBoard />}>
+              <Route
+                path="/auth/dashboard/profile"
+                element={<ProfileOverview />}
+              />
+              <Route
+                path="/auth/dashboard/notifications"
+                element={<NotficationPage />}
+              />
+              <Route
+                path="/auth/dashboard/settings"
+                element={<SettingsPage />}
+              />
+            </Route>
           </Route>
-        </Route>
-        <Route path="*" element={<FourOFourPage />}></Route>
-      </Routes>
+          <Route path="*" element={<FourOFourPage />}></Route>
+        </Routes>
+      </ThemeContext.Provider>
     </div>
   );
 }
