aaae88691eb7ec14fa5968996b23e31e96f4ac03
Gideon uwa <uwagideon092@gmail.com>
2022-09-23 12:31:59 -0700

    created_suspense-loader

---
+++ src/components/suspenseLoader/suspenseLoader.tsx
@@ -0,0 +1,21 @@
+import { ReactComponent as LogoIcon } from "../../assets/svgs/logo.svg";
+const SuspenseLoader = ({
+  width,
+  height,
+}: {
+  width: string;
+  height: string;
+}) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <LogoIcon width={width} height={height} />
+    </div>
+  );
+};
+export default SuspenseLoader;
