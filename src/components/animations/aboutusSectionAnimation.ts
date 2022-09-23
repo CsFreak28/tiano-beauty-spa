55df26feef731349ddb0954cd27d682de105f57a
Gideon uwa <uwagideon092@gmail.com>
2022-09-23 12:18:01 -0700

    adjusted_viewport-start-position-for-scroll-animation

--- src/components/animations/aboutusSectionAnimation.ts
+++ src/components/animations/aboutusSectionAnimation.ts
@@ -5,12 +5,12 @@ export function animateAboutSection(paragraph: HTMLParagraphElement) {
   let spanElements: Array<HTMLSpanElement> = [
     ...paragraph.querySelectorAll("span"),
   ];
-  gsap.to(spanElements, 1, {
+  gsap.to(spanElements, 0.8, {
     stagger: 0.1,
     opacity: 1,
     scrollTrigger: {
       trigger: paragraph,
-      start: "top 40%",
+      start: "top 60%",
     },
   });
 }
