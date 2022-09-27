a967c5b4a171eb14c48b09f9262ba150011984e5
Gideon uwa <uwagideon092@gmail.com>
2022-09-27 05:12:34 -0700

    removed_strict-mode-from-App-component

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,12 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Router>
       <ScrollToTop/>
       <App />
     </Router>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
