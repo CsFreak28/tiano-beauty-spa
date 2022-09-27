9546412e3a5a9fb9cc26b4846e036643178827e2
Gideon uwa <uwagideon092@gmail.com>
2022-09-27 05:18:59 -0700

    added_yoyo-animation-to-hero-section

--- src/components/animations/heroSectionAnimation.ts
+++ src/components/animations/heroSectionAnimation.ts
@@ -1,2 +1,10 @@
-function animateHeroSection() {}
+import gsap from "gsap";
+function animateHeroSection(arrayOfImages: Array<HTMLImageElement>) {
+  gsap.to(arrayOfImages, 1, {
+    y: "10px",
+    yoyo: true,
+    repeat: -1,
+    stagger: 0.1,
+  });
+}
 export default animateHeroSection;
